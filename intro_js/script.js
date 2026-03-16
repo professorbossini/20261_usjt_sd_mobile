11733ed6b742077adfe141040b80cc81a585ccf1
Rodrigo Bossini <professorbossini@gmail.com>
2026-03-16 19:17:34 -0300

    first commit

---
+++ intro_js/script.js
@@ -0,0 +1,82 @@
+//funções
+//function e arrow function
+// const f1 = a => a
+// // const f1 = a => {return a}
+// const f2 = () => {2}
+// console.log(f2())
+
+// console.log(f1(2))
+// const somar = (a, b) => {
+//   a + b
+//   return undefined
+// }
+// console.log(somar(2, 3))
+// const hello = nome => console.log('Oi, ' + nome)
+// hello('Ana')
+// const hello = () => {console.log('ooi')}
+// hello()
+
+
+// const triplo = function(n = 5){
+//   return n * 3
+// }
+// console.log(triplo())
+// console.log(triplo(6))
+// const dobro = function(n){
+//   return 2 * n
+// }
+// console.log(dobro(5))
+// function hello(){
+//   console.log('Oi')
+// }
+// hello(undefined)
+// function hello(nome){
+//   console.log("Oi, " + nome)
+// }
+// hello('Ana')
+//vetores ou listas
+// v1 = []
+// console.log(v1.length)
+// v1[0] = 2
+// console.log(v1.length)
+// v1[10] = 25
+// console.log(v1.length)
+// console.log(v1)
+// for(let i = 0; i < v1.length; i = i + 1){
+//   console.log(v1[i])
+// }
+
+
+//coerção
+// const n1 = 2
+// const n2 = '3'
+// const n3 = n1 + n2 //coerção implícita
+// const n4 = n1 + Number(n2) //coerção explícita
+// console.log(n3)
+// console.log(n4)
+
+
+//var, let, const
+//hoist: içamento
+// let idade = 18
+// let nome = 'João'
+// console.log(`Oi, ${nome}`)
+// if(idade >= 18){
+//   console.log(nome + ', você pode dirigir')
+// }
+// console.log("Você tem " + idade + " anos")
+// console.log('Tchau, ' + nome)
+// let linguagem = 'Javascript'
+// console.log("Aprendendo " + linguagem)
+// let linguagem = 'Python'
+// console.log('Aprendendo ' + linguagem)
+// const idade = 17
+// console.log ('Tenho ' + idade + ' anos')
+// idade = 18
+// console.log(`Agora eu tenho ${idade} anos`)
+// const nome = 'José'
+// console.log(nome)
+// nome = 'José da Silva'
+// console.log(nome)
+
+//console.log("Hello, World")
