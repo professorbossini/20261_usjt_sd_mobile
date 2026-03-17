ca5e5e0762bfbdf3482321f19469f9a334507ce7
Rodrigo Bossini <professorbossini@gmail.com>
2026-03-16 21:41:18 -0300

    comecar cap 3

--- intro_js/script.js
+++ intro_js/script.js
@@ -1,3 +1,114 @@
+// JSON: JavaScript Object Notation
+//Uma calculadora faz as quatro operações fundamentais
+//A soma deve ser uma função comum com function
+//A subtração deve ser uma arrow function com return
+//A multiplicação deve ser uma arrow function sem return
+//A divisão fica a seu critério
+// const calculadora = {
+//   somar: function(a, b){
+//     return a + b
+//   },
+//   subtrair: (a, b) => {return a - b},
+//   multiplicar: (a, b) => a * b,
+//   dividir: function(dividendo, divisor){
+//     if(divisor === 0)
+//       console.log('Não divida por zero')
+//     else
+//       return dividendo / divisor
+    
+//   }
+// }
+// const a = 2, b = 3
+// console.log(calculadora.somar(a, b))
+// console.log(calculadora.dividir(a, 0))
+
+//Uma concessionaria tem CNPJ, nome e endereço. Ela possui alguns carros em estoque. Cada carro tem marca e modelo.
+
+
+
+// const concessionaria = {
+//   cnpj: '00.000.000/0001-00',
+//   nome: 'Volks',
+//   endereco: {
+//     logradouro: 'Rua J',
+//     numero: 12,
+//     bairro: {
+//       nome: 'Vila H',
+//       cidade: 'SP'
+//     }
+//   },
+//   estoque: [
+//     {
+//       marca: "VW",
+//       modelo: "Fusca"
+//     },
+//     {
+//       marca: "Ford",
+//       modelo: 'Ka'
+//     },
+//     {
+//       marca: 'Ford',
+//       modelo: 'Fiesta'
+//     }
+//   ]
+// }
+// // console.log(concessionaria.endereco.bairro.cidade)
+// // console.log(concessionaria.estoque[0]['modelo'])
+
+// const nome = 'Ana'
+// console.log('Oi ' + nome)
+// console.log(`Oi, ${nome}`)
+
+
+// for(let veiculo of concessionaria.estoque){
+//   console.log(`
+//     Marca: ${veiculo.marca}, Modelo: ${veiculo.modelo}  
+//   `)  
+// }
+
+
+
+
+//Uma pessoa se chama Maria, tem 21 anos e mora na Rua B, número 12
+// const pessoa = {
+//   nome: 'Maria',
+//   idade: 21,
+//   endereco: {
+//     logradouro: 'Rua B',
+//     numero: 12
+//   }
+// }
+// console.log(pessoa.endereco.logradouro)
+// console.log(pessoa['endereco']['numero'])
+// console.log(pessoa['endereco'].logradouro)
+// const pessoa = {
+//   nome: 'Maria',
+//   idade: 21,
+//   logradouro: 'Rua B',
+//   numero: 12
+// }
+
+//Uma pessoa se chama João e tem 17 anos
+// const pessoa = {
+//   nome: "João",
+//   idade: 17
+// }
+// // . e []
+// console.log(pessoa.nome)
+// console.log(pessoa['idade'])
+
+// const dobrar = (n) => 2 * n
+
+// console.log(dobrar(5))
+// const hello = nome => console.log('Hello ' + nome)
+// hello('Ana')
+// const hello = () => console.log("Hello")
+// hello()
+// const hello = nome => {console.log('Oi ' + nome )}
+// hello('Ana')
+// const hello = () => { console.log('Oi')}
+// hello()
+
 //funções
 //function e arrow function
 // const f1 = a => a
