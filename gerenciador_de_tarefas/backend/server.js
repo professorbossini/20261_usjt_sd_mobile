8a566445ab5a84f44bb5b6197658a48192575fd7
Rodrigo Bossini <professorbossini@gmail.com>
2026-05-11 19:45:37 -0300

    exercicio put proposto

--- gerenciador_de_tarefas/backend/server.js
+++ gerenciador_de_tarefas/backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mysql = require('mysql2/promise')
 const app = express()
-
+app.use(express.json())//middleware
 let conexao
 
 async function conectar(){
@@ -13,8 +13,48 @@ async function conectar(){
   })
   console.log('Conectado ao MySQL!')
 }
-
 conectar()
+
+app.get('/tarefas', async (req, res) => {
+  try{
+    const [linhas] = await conexao.query('SELECT * FROM tb_tarefa')
+    res.json(linhas)
+  }
+  catch(e){
+    console.log(`Erro: ${e}`)
+    res.status(500).json({
+      erro: 'Erro ao criar a tarefa'
+    })
+  }
+})
+//fazer a rota post, ou seja, aquela que permite que uma tarefa seja cadastrada na base
+app.post('/tarefas', async (req, res) => {
+  try{
+    const sql = 'INSERT INTO tb_tarefa (titulo, descricao) VALUES(?, ?)'
+    const [resultado] = await conexao.query(
+      sql,
+      [req.body.titulo, req.body.descricao]
+    )
+    res.json({
+      cod_tarefa: resultado.insertId,
+      titulo: req.body.titulo,
+      descricao: req.body.descricao
+    })
+  }
+  catch(e){
+    console.log(`Erro: ${e}`)
+    res.status(500).json({
+      erro: 'Erro ao criar tarefa'
+    })
+  }
+})
+
+app.put('/tarefas/:id', async (req, res) => {
+  //completar esse método em 15 minutos
+})
+
+
+
 
 app.get('/', (req, res) => {
   res.json({
